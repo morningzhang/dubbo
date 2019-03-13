ca45760bc24141a86ded3f14a03c2cbb8b27bcc4
zhangliming <zhangliming@dianwoba.com>
2019-03-13 11:09:42 +0800

    1.获取所有dubbo的provider的连接，添加close方法 2.动态添加方法

--- test/zk_reg.py
+++ test/zk_reg.py
@@ -4,6 +4,6 @@ from pydubbo.pydubbo import DubboZK
 if __name__ == '__main__':
     d = DubboZK(interface="com.dianwoba.genius.provider.StaffProvider", hosts='192.168.11.29:2185')
     res = d.findByCode([("Ljava/lang/String;", u"00064")])
-    print(res)
-    res = d.findByCode([("Ljava/lang/String;", u"00064")])
+    print(res.data.name)
+    res = d.findByMobile([("Ljava/lang/String;", u"18657112153")])
     print(res.data.name)
